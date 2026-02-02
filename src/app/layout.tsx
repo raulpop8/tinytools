9e66ba05a148453ba51dfd590df08b9e52cf864b
Raul Pop <raul.pop8@gmail.com>
2026-02-02 22:48:22 +0200

    added vercel analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
 }
+
+import { Analytics } from "@vercel/analytics/next";
