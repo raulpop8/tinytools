1785f1f118b742b94d2d9571e74e57ffe43c1099
Raul Pop <raul.pop8@gmail.com>
2026-02-10 00:52:39 +0200

    added new ideads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,6 +66,30 @@ const cards = [
     },
 
     {
+      title: "Checklist",
+      description: "check your life list",
+      href: "https://neal.fun/life-checklist/",
+      tag: "Fun",
+      comingSoon: true,
+    },
+
+    {
+      title: "Progress",
+      description: "time passing by",
+      href: "https://neal.fun/progress/",
+      tag: "Fun",
+      comingSoon: true,
+    },
+
+    {
+      title: "Life Stats",
+      description: "life stats since you were born",
+      href: "https://neal.fun/life-stats/",
+      tag: "Fun",
+      comingSoon: true,
+    },
+
+    {
       title: "Idle Clicker",
       description: "practice clicking skills",
       href: "/games/",
